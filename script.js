b47d9afdae8b3375816aadf0d8f02e771a795fe1
someshtiwarii <someshtiwari96@gmail.com>
2021-08-01 23:10:15 +0530

    fixed error in js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const billAmount = document.querySelector(".bill-amount");
 const cashGiven = document.querySelector(".cash-given");
 const checkButton = document.querySelector(".check-button");
 const errorMessage = document.querySelector(".error-message");
-const noOfNotes = document.querySelector(".no-of-notes");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const notes=[2000,500,100,20,10,5,1];
 
